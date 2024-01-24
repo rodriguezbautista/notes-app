58e820dac7576c6d0ad720d25ba8f569b49360a7
Rig0bert0 <bautistarodriguez2431@gmail.com>
2024-01-24 16:29:50 -0300

    Setted accepted domains as env var

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,24 +6,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser())
+  app.use(cookieParser());
 
-  const whitelist = ['http://localhost:3000', 'http://192.168.0.129:3000']
+  const whitelist = process.env.ACCEPTED_DOMAINS.split(";");
   const corsOptions = {
     origin: function (origin, callback) {
       if (whitelist.indexOf(origin) !== -1 || !origin) {
-        callback(null, true)
+        callback(null, true);
       } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+        callback(new Error('Not allowed by CORS'));
+      };
     },
     credentials: true
-  }
+  };
 
-  app.use(cors(corsOptions))
+  app.use(cors(corsOptions));
 
-  dotenv.config()
-  
+  dotenv.config();
+
   await app.listen(8080);
 }
 
