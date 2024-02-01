314d6d763d667f48a91a6819a74e708f93694b74
Rig0bert0 <bautistarodriguez2431@gmail.com>
2024-02-01 15:03:08 -0300

    signin fixed

--- backend/src/controllers/logout.controller.ts
+++ backend/src/controllers/logout.controller.ts
@@ -5,8 +5,8 @@ import { Response } from "express";
 export class LogoutController{
   @Get()
   logout(@Res() response: Response){
-    response.clearCookie('token');
-    response.clearCookie('user');
+    response.appendHeader('Set-Cookie', `token=; httponly; SameSite=None; Secure; Expires=Thu, 01 Jan 1970 00:00:00 GMT`)
+    response.appendHeader('Set-Cookie', `user=; httponly; SameSite=None; Secure; Expires=Thu, 01 Jan 1970 00:00:00 GMT`)
     response.send()
   }
 }
