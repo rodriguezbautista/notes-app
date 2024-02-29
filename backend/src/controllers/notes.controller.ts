348997aa29be75d19fbe4b4714a943b91e9cb5bd
Rig0bert0 <bautistarodriguez2431@gmail.com>
2024-02-29 12:00:05 -0300

    Optimized getNotes

--- backend/src/controllers/notes.controller.ts
+++ backend/src/controllers/notes.controller.ts
@@ -1,13 +1,13 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Req,
-  Res,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+	Req,
+	Res,
 } from '@nestjs/common';
 import { NotesService } from '../services/notes.service';
 import { Response, Request } from 'express';
@@ -16,125 +16,125 @@ import { CategoriesService } from 'src/services/categories.service';
 
 @Controller('/notes')
 export class NotesController {
-  constructor(
-    private readonly notesService: NotesService,
-    private readonly categoriesService: CategoriesService,
-  ) {}
+	constructor(
+		private readonly notesService: NotesService,
+		private readonly categoriesService: CategoriesService,
+	) {}
 
-  @Get()
-  async getUserNotes(
-    @Req() request: Request,
-    @Res() response: Response,
-  ): Promise<Response | null> {
-    try {
-      const user = request.cookies.user;
-      const notes = await this.notesService.getNotes({ authorId: user });
+	@Get()
+	async getUserNotes(
+		@Req() request: Request,
+		@Res() response: Response,
+	): Promise<Response | null> {
+		try {
+			const user = request.cookies.user;
+			const notes = await this.notesService.getNotes({ authorId: user });
 
-      if (notes) {
-        return response.json(notes);
-      }
+			if (notes) {
+				return response.json(notes);
+			}
 
-      return response.status(401).json('Wrong credentials');
-    } catch (err) {
-      console.log(err);
-      return response.status(500).json(err);
-    }
-  }
+			return response.status(401).json('Wrong credentials');
+		} catch (err) {
+			console.log(err);
+			return response.status(500).json(err);
+		}
+	}
 
-  @Get('/:id')
-  async getUserNoteById(
-    @Req() request: Request,
-    @Param('id') id: number,
-    @Res() response: Response,
-  ): Promise<Response | null> {
-    try {
-      const user = request.cookies.user;
-      const notes = await this.notesService.getNote({
-        authorId: user,
-        id,
-      });
+	@Get('/:id')
+	async getUserNoteById(
+		@Req() request: Request,
+		@Param('id') id: number,
+		@Res() response: Response,
+	): Promise<Response | null> {
+		try {
+			const user = request.cookies.user;
+			const notes = await this.notesService.getNote({
+				authorId: user,
+				id,
+			});
 
-      if (notes) {
-        return response.json(notes);
-      }
+			if (notes) {
+				return response.json(notes);
+			}
 
-      return response.status(401).json('Wrong credentials');
-    } catch (err) {
-      console.log(err);
-      return response.status(500).json(err);
-    }
-  }
+			return response.status(401).json('Wrong credentials');
+		} catch (err) {
+			console.log(err);
+			return response.status(500).json(err);
+		}
+	}
 
-  @Post()
-  async post(
-    @Req() request: Request,
-    @Body() body: Prisma.NotesCreateInput,
-    @Res() response: Response,
-  ): Promise<Response | null> {
-    try {
-      body['authorId'] = request.cookies.user;
-      const note = await this.notesService.createNote(body);
+	@Post()
+	async post(
+		@Req() request: Request,
+		@Body() body: Prisma.NotesCreateInput,
+		@Res() response: Response,
+	): Promise<Response | null> {
+		try {
+			body['authorId'] = request.cookies.user;
+			const note = await this.notesService.createNote(body);
 
-      if (note) {
-        return response.status(201).json(note);
-      }
+			if (note) {
+				return response.status(201).json(note);
+			}
 
-      return response.status(401).json('Wrong credentials');
-    } catch (err) {
-      console.log(err);
-      return response.status(500).json(err);
-    }
-  }
+			return response.status(401).json('Wrong credentials');
+		} catch (err) {
+			console.log(err);
+			return response.status(500).json(err);
+		}
+	}
 
-  @Patch('/:id')
-  async edit(
-    @Body() body: Prisma.NotesUpdateInput,
-    @Param('id') id: number,
-    @Res() response: Response,
-  ): Promise<Response | null> {
-    try {
-      const { content, status, categories } = body;
+	@Patch('/:id')
+	async edit(
+		@Body() body: Prisma.NotesUpdateInput,
+		@Param('id') id: number,
+		@Res() response: Response,
+	): Promise<Response | null> {
+		try {
+			const { content, status, categories } = body;
 
-      const post = await this.notesService.updateNote({
-        data: {
-          content,
-          status,
-          categories: {
-            deleteMany: {
-              category: {
-                notIn: categories as string[],
-              },
-            },
-            createMany: {
-              data: (categories as string[]).map((c) => ({ category: c })),
-            },
-          },
-        },
-        where: {
-          id,
-        },
-      });
+			const post = await this.notesService.updateNote({
+				data: {
+					content,
+					status,
+					categories: {
+						deleteMany: {
+							category: {
+								notIn: categories as string[],
+							},
+						},
+						createMany: {
+							data: (categories as string[])?.map(c => ({ category: c })),
+						},
+					},
+				},
+				where: {
+					id,
+				},
+			});
 
-      return response.json(post);
-    } catch (err) {
-      console.log(err);
-      return response.status(500).json(err);
-    }
-  }
+			return response.json(post);
+		} catch (err) {
+			console.log(err);
+			return response.status(500).json(err);
+		}
+	}
 
-  @Delete('/:id')
-  async delete(
-    @Param('id') id: number,
-    @Res() response: Response,
-  ): Promise<Response | null> {
-    try {
-      await this.notesService.deleteNote({
-        id,
-      });
-      return response.status(204).send();
-    } catch (err) {
-      console.log(err);
-      return response.status(500).json(err);
-    }
-  }
+	@Delete('/:id')
+	async delete(
+		@Param('id') id: number,
+		@Res() response: Response,
+	): Promise<Response | null> {
+		try {
+			await this.notesService.deleteNote({
+				id,
+			});
+			return response.status(204).send();
+		} catch (err) {
+			console.log(err);
+			return response.status(500).json(err);
+		}
+	}
 }
--- backend/src/services/notes.service.ts
+++ backend/src/services/notes.service.ts
@@ -1,94 +1,82 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { Notes, Prisma } from '@prisma/client';
+import { Notes, Prisma, PrismaPromise } from '@prisma/client';
 
 @Injectable()
 export class NotesService {
-  constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
 
-  async getNote(
-    notesWhereUniqueInput: Prisma.NotesWhereUniqueInput,
-  ): Promise<Notes | null> {
-    return this.prisma.notes
-      .findUnique({
-        where: notesWhereUniqueInput,
-        include: {
-          categories: {
-            select: {
-              category: true,
-            },
-          },
-        },
-      })
-      .then((note) => ({
-        ...note,
-        categories: note.categories.map((c) => c.category),
-      }));
-  }
+	async getNote(
+		notesWhereUniqueInput: Prisma.NotesWhereUniqueInput,
+	): Promise<Notes | null> {
+		return this.prisma.notes
+			.findUnique({
+				where: notesWhereUniqueInput,
+				include: {
+					categories: {
+						select: {
+							category: true,
+						},
+					},
+				},
+			})
+			.then(note => ({
+				...note,
+				categories: note.categories.map(c => c.category),
+			}));
+	}
 
-  async getNotes(
-    notesWhereInput: Prisma.NotesWhereInput,
-  ): Promise<Notes[] | null> {
-    return this.prisma.notes
-      .findMany({
-        where: notesWhereInput,
-        orderBy: {
-          lastModified: 'desc',
-        },
-        include: {
-          categories: {
-            select: {
-              category: true,
-            },
-          },
-        },
-      })
-      .then((notes) =>
-        notes.map((note) => ({
-          ...note,
-          categories: note.categories.map((c) => c.category),
-        })),
-      );
-  }
+	async getNotes(
+		notesWhereInput: Prisma.NotesWhereInput,
+	): Promise<Notes[] | null> {
+		return this.prisma.$queryRaw`
+      SELECT n.*, ARRAY_REMOVE(ARRAY_AGG(c.category), NULL) AS categories 
+      FROM "Notes" n
+      LEFT JOIN "Categories" c ON c."noteId" = n.id
+      WHERE "authorId" = ${notesWhereInput.authorId}
+      GROUP BY n.id
+      ORDER BY n."lastModified" DESC;
+      ` as PrismaPromise<Notes[]>;
+	}
 
-  async createNote(data: Prisma.NotesCreateInput): Promise<Notes> {
-    return this.prisma.notes.create({
-      data,
-    });
-  }
+	async createNote(data: Prisma.NotesCreateInput): Promise<Notes> {
+		return this.prisma.notes.create({
+			data,
+		});
+	}
 
-  async updateNote(params: {
-    where: Prisma.NotesWhereUniqueInput;
-    data: Prisma.NotesUpdateInput;
-  }): Promise<Notes> {
-    const { where, data } = params;
+	async updateNote(params: {
+		where: Prisma.NotesWhereUniqueInput;
+		data: Prisma.NotesUpdateInput;
+	}): Promise<Notes> {
+		const { where, data } = params;
 
-    return this.prisma.notes
-      .update({
-        data,
-        where,
-        include: {
-          categories: {
-            select: {
-              category: true,
-            },
-          },
-        },
-      })
-      .then((note) => ({
-        ...note,
-        categories: note.categories.map((c) => c.category),
-      }));
-  }
+		return this.prisma.notes
+			.update({
+				data,
+				where,
+				include: {
+					categories: {
+						select: {
+							category: true,
+						},
+					},
+				},
+			})
+			.then(note => ({
+				...note,
+				categories: note.categories.map(c => c.category),
+			}));
+	}
 
-  async deleteNote(where: Prisma.NotesWhereUniqueInput | any): Promise<Notes> {
-    await this.prisma.categories.deleteMany({
-      where: {
-        noteId: where.id,
-      },
-    });
-    return this.prisma.notes.delete({
-      where,
-    });
-  }
+	async deleteNote(where: Prisma.NotesWhereUniqueInput | any): Promise<Notes> {
+		await this.prisma.categories.deleteMany({
+			where: {
+				noteId: where.id,
+			},
+		});
+		return this.prisma.notes.delete({
+			where,
+		});
+	}
 }
