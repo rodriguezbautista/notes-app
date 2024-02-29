5a1b810e1d2f46a94f29d7b04e38e01db7e62cb6
Rig0bert0 <bautistarodriguez2431@gmail.com>
2024-02-29 12:00:56 -0300

    style improvement

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,161 +1,194 @@
-import React, { useEffect, useRef, useState } from "react";
-import Categories from "./Categories.tsx";
-import NoteActions from "./NoteActions.tsx";
-import { useNavigate } from "react-router-dom";
-import url from "../utils/apiUrl.js";
+import React, { useEffect, useRef, useState } from 'react';
+import Categories from './Categories.tsx';
+import NoteActions from './NoteActions.tsx';
+import { useNavigate } from 'react-router-dom';
+import url from '../utils/apiUrl.js';
 
 export interface NoteProps {
-  id: string;
-  content: string;
-  lastModified: string;
-  categories: string[];
-  status: string;
+	id: string;
+	content: string;
+	lastModified: string;
+	categories: string[];
+	status: string;
 }
 
-export default function Note({ id, content, lastModified, categories, status }: NoteProps) {
-  const [actionsOpened, setActionsOpened] = useState(false);
-  const [isEditing, setIsEditing] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isLarge, setIsLarge] = useState(false);
-  const [isReadingMore, setIsReadingMore] = useState(false);
-  const [localeContent, setLocaleContent] = useState(content);
-  const [localeCategories, setLocaleCategories] = useState(categories);
-  const navigate = useNavigate();
-  const optionsRef = useRef<HTMLDivElement>(null);
-  const contentRef = useRef<HTMLParagraphElement>(null);
+export default function Note({
+	id,
+	content,
+	lastModified,
+	categories,
+	status,
+}: NoteProps) {
+	const [actionsOpened, setActionsOpened] = useState(false);
+	const [isEditing, setIsEditing] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isLarge, setIsLarge] = useState(false);
+	const [isReadingMore, setIsReadingMore] = useState(false);
+	const [localeContent, setLocaleContent] = useState(content);
+	const [localeCategories, setLocaleCategories] = useState(categories);
+	const navigate = useNavigate();
+	const optionsRef = useRef<HTMLDivElement>(null);
+	const contentRef = useRef<HTMLParagraphElement>(null);
 
-  const lastModifiedFormatted = new Date(lastModified).toLocaleDateString("en-US", {
-    year: "2-digit",
-    month: "numeric",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+	const lastModifiedFormatted = new Date(lastModified).toLocaleDateString(
+		'en-US',
+		{
+			year: '2-digit',
+			month: 'numeric',
+			day: 'numeric',
+			hour: '2-digit',
+			minute: '2-digit',
+		},
+	);
 
-  async function editNote() {
-    try {
-      setIsLoading(true);
-      const response = await fetch(url + `/notes/${id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          content: localeContent,
-          categories: localeCategories.map((c) => {
-            return { category: c };
-          }),
-        }),
-      });
-      setIsLoading(false);
-      if (response.ok) {
-        navigate(0);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	async function editNote() {
+		try {
+			setIsLoading(true);
+			const response = await fetch(url + `/notes/${id}`, {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				credentials: 'include',
+				body: JSON.stringify({
+					content: localeContent,
+					categories: localeCategories.map(c => {
+						return { category: c };
+					}),
+				}),
+			});
+			setIsLoading(false);
+			if (response.ok) {
+				navigate(0);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-  function compareCategories(categories, localCategories) {
-    return (
-      categories.length === localCategories.length &&
-      categories.every((c) => localCategories.includes(c))
-    );
-  }
+	function compareCategories(categories, localCategories) {
+		return (
+			categories.length === localCategories.length &&
+			categories.every(c => localCategories.includes(c))
+		);
+	}
 
-  // tag note as large depending on content height
-  useEffect(() => {
-    if (contentRef.current?.offsetHeight && contentRef.current?.offsetHeight > 300)
-      setIsLarge(true);
-  }, []);
+	// tag note as large depending on content height
+	useEffect(() => {
+		if (
+			contentRef.current?.offsetHeight &&
+			contentRef.current?.offsetHeight > 300
+		)
+			setIsLarge(true);
+	}, []);
 
-  // after opening the note actions, any click outside will close it
-  useEffect(() => {
-    if (actionsOpened) {
-      optionsRef.current?.focus();
-    }
-  }, [actionsOpened]);
+	useEffect(() => {
+		if (contentRef.current && contentRef.current.parentElement) {
+			if (isReadingMore)
+				contentRef.current.parentElement.style.maxHeight =
+					String(contentRef.current?.offsetHeight + 64) + 'px';
+			else contentRef.current.parentElement.style.maxHeight = '250px';
+		}
+	}, [isReadingMore]);
 
-  return (
-    <li className="note">
-      <div className="note__header">
-        <span className="note__created-at">{lastModifiedFormatted}</span>
-        <button
-          className="note__actions-btn"
-          onClick={() => {
-            setActionsOpened(!actionsOpened);
-          }}
-        >
-          <img src="" alt="" />
-          •••
-        </button>
-        <div
-          ref={optionsRef}
-          className={`note__actions${+actionsOpened ? " opened" : ""}`}
-          tabIndex={0}
-          style={{ visibility: actionsOpened ? "visible" : "hidden" }}
-          onBlur={(e) => {
-            if (e.currentTarget !== e.relatedTarget?.parentElement) setActionsOpened(false);
-          }}
-        >
-          <NoteActions status={status} id={id} setIsEditing={setIsEditing} />
-        </div>
-      </div>
-      <div
-        className={`note__content__wrapper${
-          isLarge ? (isReadingMore ? " expanded" : " contracted") : ""
-        }`}
-      >
-        <p ref={contentRef} className={`note__content`}>
-          {content}
-        </p>
-      </div>
-      {!isLarge ? (
-        <></>
-      ) : isReadingMore ? (
-        <button className="note__read-more" onClick={() => setIsReadingMore(!isReadingMore)}>
-          Read Less...
-        </button>
-      ) : (
-        <button className="note__read-more" onClick={() => setIsReadingMore(!isReadingMore)}>
-          Read More...
-        </button>
-      )}
-      {!!categories.length && (
-        <Categories categories={categories} setCategories={setLocaleCategories} />
-      )}
-      {isEditing && (
-        <>
-          <button
-            className="note__action primary"
-            // button is only disabled when content and category list hasnt changed
-            disabled={
-              (content === localeContent && compareCategories(categories, localeCategories)) ||
-              isLoading
-            }
-            onClick={async () => {
-              setLocaleContent(content);
-              setIsEditing(false);
-              await editNote();
-            }}
-          >
-            Save
-            <img src="/check-icon.svg" alt="Submit edit button" className="icon action__icon" />
-          </button>
-          <button
-            className="note__action primary"
-            disabled={isLoading}
-            onClick={() => {
-              setLocaleContent(content);
-              setIsEditing(false);
-            }}
-          >
-            Cancel
-            <img src="/xmark-icon.svg" alt="Submit edit button" className="icon action__icon" />
-          </button>
-        </>
-      )}
-    </li>
-  );
+	// after opening the note actions, any click outside will close it
+	useEffect(() => {
+		if (actionsOpened) {
+			optionsRef.current?.focus();
+		}
+	}, [actionsOpened]);
+
+	return (
+		<li className="note">
+			<div className="note__header">
+				<span className="note__created-at">{lastModifiedFormatted}</span>
+				<button
+					className="note__actions-btn"
+					onClick={() => {
+						setActionsOpened(!actionsOpened);
+					}}>
+					<img src="" alt="" />
+					•••
+				</button>
+				<div
+					ref={optionsRef}
+					className={`note__actions${+actionsOpened ? ' opened' : ''}`}
+					tabIndex={0}
+					style={{ visibility: actionsOpened ? 'visible' : 'hidden' }}
+					onBlur={e => {
+						if (e.currentTarget !== e.relatedTarget?.parentElement)
+							setActionsOpened(false);
+					}}>
+					<NoteActions status={status} id={id} setIsEditing={setIsEditing} />
+				</div>
+			</div>
+			<div
+				className={`note__content__wrapper${
+					isLarge ? (isReadingMore ? ' expanded' : ' contracted') : ''
+				}`}>
+				<p ref={contentRef} className={`note__content`}>
+					{content}
+				</p>
+				{!isLarge ? (
+					<></>
+				) : isReadingMore ? (
+					<button
+						className="note__read-more"
+						onClick={() => setIsReadingMore(!isReadingMore)}>
+						Read Less...
+					</button>
+				) : (
+					<button
+						className="note__read-more"
+						onClick={() => setIsReadingMore(!isReadingMore)}>
+						Read More...
+					</button>
+				)}
+			</div>
+			{!!categories.length && (
+				<Categories
+					categories={categories}
+					setCategories={setLocaleCategories}
+				/>
+			)}
+			{isEditing && (
+				<>
+					<button
+						className="note__action primary"
+						// button is only disabled when content and category list hasnt changed
+						disabled={
+							(content === localeContent &&
+								compareCategories(categories, localeCategories)) ||
+							isLoading
+						}
+						onClick={async () => {
+							setLocaleContent(content);
+							setIsEditing(false);
+							await editNote();
+						}}>
+						Save
+						<img
+							src="/check-icon.svg"
+							alt="Submit edit button"
+							className="icon action__icon"
+						/>
+					</button>
+					<button
+						className="note__action primary"
+						disabled={isLoading}
+						onClick={() => {
+							setLocaleContent(content);
+							setIsEditing(false);
+						}}>
+						Cancel
+						<img
+							src="/xmark-icon.svg"
+							alt="Submit edit button"
+							className="icon action__icon"
+						/>
+					</button>
+				</>
+			)}
+		</li>
+	);
 }
--- frontend/src/components/NoteActions.tsx
+++ frontend/src/components/NoteActions.tsx
@@ -1,131 +1,138 @@
-import { useNavigate } from "react-router-dom";
-import url from "../utils/apiUrl";
-import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import url from '../utils/apiUrl';
+import React, { useState } from 'react';
 
 interface NoteActionsProps {
-  id: string;
-  status: string;
-  setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
+	id: string;
+	status: string;
+	setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function NoteActions({ id, status, setIsEditing }: NoteActionsProps) {
-  const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
+export default function NoteActions({
+	id,
+	status,
+	setIsEditing,
+}: NoteActionsProps) {
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
 
-  async function deleteNote() {
-    try {
-      setIsLoading(true);
-      await fetch(url + `/notes/${id}`, {
-        method: "DELETE",
-        credentials: "include",
-      });
-      setIsLoading(false);
-      navigate(0);
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	async function deleteNote() {
+		try {
+			setIsLoading(true);
+			await fetch(url + `/notes/${id}`, {
+				method: 'DELETE',
+				credentials: 'include',
+			});
+			setIsLoading(false);
+			navigate(0);
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-  async function newStatus(newStatus) {
-    await fetch(url + `/notes/${id}`, {
-      method: "PATCH",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        status: newStatus,
-      }),
-    });
-    navigate(0);
-  }
+	async function newStatus(newStatus) {
+		await fetch(url + `/notes/${id}`, {
+			method: 'PATCH',
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				status: newStatus,
+			}),
+		});
+		navigate(0);
+	}
 
-  return (
-    <>
-      {status === "active" ? (
-        <>
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              newStatus("inactive");
-            }}
-            className="note__action"
-          >
-            Mark note as Inactive
-          </button>
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              newStatus("archived");
-            }}
-            className="note__action"
-          >
-            Mark note as Archived
-          </button>
-        </>
-      ) : status === "inactive" ? (
-        <>
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              newStatus("active");
-            }}
-            className="note__action"
-          >
-            Mark note as Active
-          </button>
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              newStatus("archived");
-            }}
-            className="note__action"
-          >
-            Mark note as Archived
-          </button>
-        </>
-      ) : (
-        <>
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              newStatus("active");
-            }}
-            className="note__action"
-          >
-            Mark note as Active
-          </button>
-          <button
-            onClick={async (e) => {
-              e.preventDefault();
-              newStatus("inactive");
-            }}
-            className="note__action"
-          >
-            Mark note as Inactive
-          </button>
-        </>
-      )}
-      <button
-        className="note__action"
-        disabled={isLoading}
-        onClick={() => {
-          setIsEditing(true);
-        }}
-      >
-        <img src="/edit-icon.svg" alt="Note edit button" className="action__icon" />
-        <span>Edit</span>
-      </button>
-      <button
-        className="note__action"
-        disabled={isLoading}
-        onClick={async (e) => {
-          await deleteNote();
-        }}
-      >
-        <img src="/delete-icon.svg" alt="Note delete button" className="action__icon" />
-        <span>Delete</span>
-      </button>
-    </>
-  );
+	return (
+		<>
+			{status === 'active' ? (
+				<>
+					<button
+						onClick={async e => {
+							e.preventDefault();
+							newStatus('inactive');
+						}}
+						className="note__action">
+						Mark note as Inactive
+					</button>
+					<button
+						onClick={async e => {
+							e.preventDefault();
+							newStatus('archived');
+						}}
+						className="note__action">
+						Mark note as Archived
+					</button>
+				</>
+			) : status === 'inactive' ? (
+				<>
+					<button
+						onClick={async e => {
+							e.preventDefault();
+							newStatus('active');
+						}}
+						className="note__action">
+						Mark note as Active
+					</button>
+					<button
+						onClick={async e => {
+							e.preventDefault();
+							newStatus('archived');
+						}}
+						className="note__action">
+						Mark note as Archived
+					</button>
+				</>
+			) : (
+				<>
+					<button
+						onClick={async e => {
+							e.preventDefault();
+							newStatus('active');
+						}}
+						className="note__action">
+						Mark note as Active
+					</button>
+					<button
+						onClick={async e => {
+							e.preventDefault();
+							newStatus('inactive');
+						}}
+						className="note__action">
+						Mark note as Inactive
+					</button>
+				</>
+			)}
+			<br />
+			<hr />
+			<br />
+			<button
+				className="note__action"
+				disabled={isLoading}
+				onClick={() => {
+					setIsEditing(true);
+				}}>
+				<img
+					src="/edit-icon.svg"
+					alt="Note edit button"
+					className="action__icon"
+				/>
+				<span>Edit</span>
+			</button>
+			<button
+				className="note__action"
+				disabled={isLoading}
+				onClick={async e => {
+					await deleteNote();
+				}}>
+				<img
+					src="/delete-icon.svg"
+					alt="Note delete button"
+					className="action__icon"
+				/>
+				<span>Delete</span>
+			</button>
+		</>
+	);
 }
